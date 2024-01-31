db028a10ecf373db240066609085578bd0a55bda
AqibQasim <muhammad44aqib@gmail.com>
2024-01-31 17:24:55 +0500

    Contact Section changes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -19,7 +19,7 @@ const ContactUs = () => {
       e.preventDefault();
   
       try {
-        const response = await fetch('http://localhost:5000/', {
+        const response = await fetch('http://coventech-website-backend.eba-buqmurzx.us-east-2.elasticbeanstalk.com/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
