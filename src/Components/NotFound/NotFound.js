5321c1f045f0efa942e4a07d17b20840dc570932
AqibQasim <muhammad44aqib@gmail.com>
2024-02-07 14:50:48 +0500

    fix issues

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import { Link } from 'react-router-dom';
+
 const NotFound = () =>{
     return(
         <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
@@ -12,6 +13,7 @@ const NotFound = () =>{
 	<div className="bg-secondary px-2 text-sm rounded rotate-12 absolute">
 		Page Not Found
 	</div>
+    <Link to="/">
 	<button className="mt-5">
       <a
         className="relative inline-block text-sm font-medium text-primary group active:text-orange-500 focus:outline-none focus:ring"
@@ -21,10 +23,11 @@ const NotFound = () =>{
         ></span>
 
         <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-        <Link to="/" >Go Home</Link>
+            Go Home
         </span>
       </a>
     </button>
+    </Link>
     </main>
     )
 }
